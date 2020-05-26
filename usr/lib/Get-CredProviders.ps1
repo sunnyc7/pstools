33597d5b6105f5e185eff4700816113db3478e7a
gregzakh <grishanz@yandex.ru>
2020-05-27 01:05:42 +0500

    M pstools.psd1 A  usr/lib/Get-CredProviders.ps1

---
+++ usr/lib/Get-CredProviders.ps1
@@ -0,0 +1,20 @@
+function Get-CredProviders {
+  [CmdletBinding()]param()
+
+  end {
+    $root = 'HKLM:\SOFTWARE'
+    ($clsids = Get-ChildItem ("$root\Microsoft\Windows\CurrentVersion" +
+                               "\Authentication\Credential Providers")).ForEach{
+      $clsid, $name = $_.PSChildName, $_.GetValue('')
+      if (($prov = Get-ChildItem "$root\Classes\CLSID\$clsid" -ErrorAction 0)) {
+        [PSCustomObject]@{
+          CLSID = $clsid
+          Name = $name
+          Path = $prov.GetValue('')
+        }
+        $prov.Dispose()
+      }
+    }
+    $clsids.Dispose()
+  }
+}
