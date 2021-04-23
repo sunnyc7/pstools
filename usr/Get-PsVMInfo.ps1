d52d01d044c2c40aa2d32e81c154e9f181136407
gregzakh <grishanz@yandex.com>
2021-04-24 00:01:33 +0500

    M usr/Get-PsVMInfo.ps1

--- usr/Get-PsVMInfo.ps1
+++ usr/Get-PsVMInfo.ps1
@@ -1,3 +1,5 @@
+using namespace System.Runtime.InteropServices
+
 Set-Alias -Name vadump -Value Get-PsVMInfo
 function Get-PsVMInfo {
   [CmdletBinding()]param($PSBoundParameters)
@@ -28,13 +30,13 @@ function Get-PsVMInfo {
       MEM_COMMIT  = 0x00001000
       MEM_RESERVE = 0x00002000
       MEM_FREE    = 0x00010000
-    } -Type ([UInt32]) #-Flags
+    } -Type ([UInt32])
 
     New-Enum MEM_TYPE {
       MEM_PRIVATE = 0x00020000
       MEM_MAPPED  = 0x00040000
       MEM_IMAGE   = 0x01000000
-    } -Type ([UInt32]) #-Flags
+    } -Type ([UInt32])
 
     New-Structure MEMEORY_BASIC_INFORMATION {
       IntPtr  BaseAddress
@@ -49,6 +51,7 @@ function Get-PsVMInfo {
 
     New-Delegate ntdll {
       int NtQueryVirtualMemory([ptr, ptr, uint, _out_, uint, buf])
+      int NtReadVirtualMemory([ptr, ptr, buf, uint, buf])
     }
 
     $fmt, $to_i, $to_u = "{0:X$(($sz = [IntPtr]::Size) * 2)}", "ToInt$($sz * 8)", "ToUInt$($sz * 8)"
@@ -58,23 +61,56 @@ function Get-PsVMInfo {
   process {}
   end {
     New-PsProxy $PSBoundParameters -Callback {
-      $out = [MEMEORY_BASIC_INFORMATION]::new()
+      $out, $buf, $pnt = [MEMEORY_BASIC_INFORMATION]::new(), [Byte[]]::new(0x400), @{}
+      $local:hndl = $_.Handle
+      $local:_pid = $_.Id
+      $pnt[([IntPtr]0x7FFE0000).$to_i()] = 'User Shared Data'
+      foreach ($module in $_.Modules) {
+        $pnt[($ba = $module.BaseAddress.$to_i())] = $module.ModuleName
+        if (($nts = $ntdll.NtReadVirtualMemory.Invoke($hndl, $module.BaseAddress, $buf, $buf.Length, $null)) -ne 0) {
+          Write-Verbose (ConvertTo-ErrMessage -NtStatus $nts)
+          continue
+        }
+
+        $gch = [GCHandle]::Alloc($buf, [GCHandleType]::Pinned)
+        $ifh = [Marshal]::ReadInt32(($adr = $gch.AddrOfPinnedObject()), 0x3C)
+        $sig = [BitConverter]::ToUInt16( # getting section names and their addresses
+          [BitConverter]::GetBytes([Marshal]::ReadInt16($adr, $ifh + 0x04)), 0
+        ), [Marshal]::ReadInt16($adr, $ifh + 0x06)
+        $adr = $adr.$to_i() + $ifh + $(switch ($sig[0]) {0x014C {0x0F8} 0x8664 {0x108}})
+        (0..($sig[1] - 1)).ForEach{
+          $pnt[($ba + [Marshal]::ReadInt32([IntPtr]$adr, 0x0C))] = [Marshal]::PtrToStringAnsi([IntPtr]$adr, 0x08)
+          $adr += 0x28
+        }
+        $gch.Free()
+      }
 
       while (1) {
-        if (($nts = $ntdll.NtQueryVirtualMemory.Invoke($_.Handle, $ptr, 0, [ref]$out, $sz, $null)) -ne 0) {
+        if (($nts = $ntdll.NtQueryVirtualMemory.Invoke($hndl, $ptr, 0, [ref]$out, $sz, $null)) -ne 0) {
           Write-Verbose (ConvertTo-ErrMessage -NtStatus $nts)
           break
         }
+        $buf.Clear()
+        if (($nts = $ntdll.NtReadVirtualMemory.Invoke($hndl, $ptr, $buf, $buf.Length, $null)) -ne 0) {
+          Write-Verbose (ConvertTo-ErrMessage -NtStatus $nts)
+        }
+        if (!$pnt.ContainsKey(($sig = $ptr.$to_i()))) {
+          $pnt[$sig] = "[$(-join$buf[0..15].ForEach{$_ -in (33..122) ? [Char]$_ : '.'})]"
+        }
+
         $dmp = [PSCustomObject]@{
           BaseAddress = $fmt -f ($ba = $out.BaseAddress.$to_i())
           EndAddress = $fmt -f ($ea = $ba + $out.RegionSize.$to_u())
-          AllocationProtect = $out.AllocationProtect
+          Type = $out.Type
           State = $out.State
           Protect = $out.Protect
-          Type = $out.Type
+          Point = $pnt[$ba]
         }
         $local:ptr = [IntPtr]$ea
-        if ($query) { Add-Member -InputObject $dmp -MemberType NoteProperty -Name PID -Value $_.Id }
+        if ($query) {
+          Add-Member -InputObject $dmp -MemberType NoteProperty -Name PID -Value $_pid
+          Format-Hex -InputObject $buf
+        }
         $dmp
 
         if ($query) {
