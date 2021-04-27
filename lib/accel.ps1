8018e47b4d91760c47a855ef1808a97bbb847d2b
gregzakh <grishanz@yandex.com>
2021-04-27 20:57:27 +0500

    M lib/accel.ps1  M pstools.psd1

--- lib/accel.ps1
+++ lib/accel.ps1
@@ -1,4 +1,4 @@
-$keys, $types = ($x = [PSObject].Assembly.GetType(
+($keys, $types = ($x = [PSObject].Assembly.GetType(
   'System.Management.Automation.TypeAccelerators'
 ))::Get.Keys, @{
   buf   = [Byte[]]
@@ -7,9 +7,13 @@ $keys, $types = ($x = [PSObject].Assembly.GetType(
   ptr_  = [IntPtr].MakeByRefType()
   uint_ = [UInt32].MakeByRefType()
   sfh   = [Microsoft.Win32.SafeHandles.SafeFileHandle]
-}
-$types.Keys.ForEach{if ($_ -notin $keys) {$x::Add($_, $types.$_)}}
+})[1].Keys.ForEach{$_ -notin $keys ? $x::Add($_, $types.$_) : $null}
 
-Add-Member -InputObject ([buf]) -Name Uni -MemberType ScriptMethod -Value {
-  param([String]$str) [Text.Encoding]::Unicode.GetBytes($str)
-} -Force
+($scheme = @{
+  StrA = {param([Byte[]]$buf) [Text.Encoding]::Ascii.GetString($buf)}
+  StrU = {param([Byte[]]$buf) [Text.Encoding]::Unicode.GetString($buf)}
+  Ansi = {param([String]$str) [Text.Encoding]::Ascii.GetBytes($str)}
+  Uni  = {param([String]$str) [Text.Encoding]::Unicode.GetBytes($str)}
+}).Keys.ForEach{
+  Add-Member -InputObject ([buf]) -Name $_ -MemberType ScriptMethod -Value $scheme[$_] -Force
+}
